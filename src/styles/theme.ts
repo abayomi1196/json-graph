e98965bb61836414764fbfc8b75300f4f21158cb
Abayomi Ayodele <samabayomi301@gmail.com>
2022-10-01 09:05:22 +0100

    setup base theme config

---
+++ src/styles/theme.ts
@@ -0,0 +1,6 @@
+export const deviceSizes = {
+  mobile: `(min-width: 425px)`,
+  tablet: `(min-width: 768px)`,
+  laptop: `(min-width: 1024px)`,
+  desktop: `(min-width: 1440px)`
+};
---
+++ styled.d.ts
@@ -0,0 +1,7 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    // repeat colors & bgs here for intellisense benefits
+  }
+}
