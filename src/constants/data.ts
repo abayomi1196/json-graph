2b8f5d0a1cd39dc9cc534eb4a660feec4ca807d8
Abayomi Ayodele <samabayomi301@gmail.com>
2022-10-22 21:41:47 +0100

    update default JSON value

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,39 +1,36 @@
-// https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json
-
 const sampleJson = Object.freeze({
-  squadName: "Super Hero Squad",
-  homeTown: "Metro City",
-  formed: 2016,
-  secretBase: "Super tower",
-  active: true,
-  members: [
+  title: "Shingeki no Kyojin: The Final Season Part 2",
+  source: "MANGA",
+  format: "TV",
+  episodeDuration: "24 mins.",
+  characters: [
     {
-      name: "Molecule Man",
+      name: "Levi -  リヴァイ",
       age: 29,
-      secretIdentity: "Dan Jukes",
-      powers: ["Radiation resistance", "Turning tiny", "Radiation blast"]
+      height: "160cm (5'3)",
+      affiliations: "Scouting Legion"
     },
     {
-      name: "Madame Uppercut",
-      age: 39,
-      secretIdentity: "Jane Wilson",
-      powers: [
-        "Million tonne punch",
-        "Damage resistance",
-        "Superhuman reflexes"
-      ]
+      name: "Eren Yeager - エレン・イェーガー",
+      age: 16,
+      height: "170cm (5'7)"
     },
     {
-      name: "Eternal Flame",
-      age: 1000000,
-      secretIdentity: "Unknown",
-      powers: [
-        "Immortality",
-        "Heat Immunity",
-        "Inferno",
-        "Teleportation",
-        "Interdimensional travel"
-      ]
+      name: "Mikasa Ackerman - ミカサ・アッカーマン",
+      age: 16,
+      height: "170cm (5'7)",
+      Position: "104th Training Corps, Scouting Legion"
+    },
+    {
+      name: "Armin Arlert - アルミン・アルレルト",
+      age: 15,
+      height: "163cm (5'4)",
+      Position: "104th Training Squad, Scouting Legion"
+    },
+    {
+      name: "Hange Zoe - ハンジ・ゾエ",
+      height: "170cm (5'7)",
+      Position: "Scouting Legion"
     }
   ]
 });
